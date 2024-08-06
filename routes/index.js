392b184a88fcf03ab097eacffcaad4a4f115c25e
Helen Ndunge Kilolo <140470224+helenkilolo@users.noreply.github.com>
2024-08-06 23:57:12 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -17,6 +17,7 @@ router.get('/users/me', AuthController.getMe);
 router.post('/files', FilesController.postUpload);
 router.get('/files/:id', FilesController.getShow);
 router.get('/files', FilesController.getIndex);
+router.get('/files/:id/data', FilesController.getFile);
 router.put('/files/:id/publish', FilesController.putPublish);
 router.put('/files/:id/unpublish', FilesController.putUnpublish);
 
