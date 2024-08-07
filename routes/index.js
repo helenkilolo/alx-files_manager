dc9678eb940ef2f8e9966f9214b801846f6f79d8
helen kilolo <helenkilolo@gmail.com>
2024-08-07 03:31:14 -0700

    more updates

--- routes/index.js
+++ routes/index.js
@@ -16,6 +16,7 @@ router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', AuthController.getMe);
 router.post('/files', FilesController.postUpload);
 router.get('/files/:id', FilesController.getShow);
+router.post('/files', FilesController.postUpload);
 router.get('/files', FilesController.getIndex);
 router.get('/files/:id/data', FilesController.getFile);
 router.put('/files/:id/publish', FilesController.putPublish);
